90d68bdabe8552c2ed79ed379f8fcc594b811e8e
PSpudi <pannaspiderka@gmail.com>
2024-11-10 19:35:56 +0100

    styling, some airlines provided

--- js/airlines.js
+++ js/airlines.js
@@ -1,4 +1,15 @@
+// ['ICAO', 'Callsign', 'Airlines', 'Country code']
 let airlines = [
-    ['AAC', 'ASIANA CARGO', 'Asiana Airlines Cargo'],
-    ['AAH', 'ALOHA', 'Aloha Air Cargo']
+    ['AAC', 'ASIANA CARGO', 'Asiana Airlines Cargo', 'KOR'],
+    ['AAF', 'AIGLE AZUR', 'Aigle Azur', 'FRA'],
+    ['AAH', 'ALOHA', 'Aloha Air Cargo', 'USA'],
+    ['AAL', 'AMERICAN', 'American Airlines', 'USA'],
+    ['AAN','ANSETT','Ansett Australia','AUS'],
+    ['AAR','ASIANA','Asiana Airlines','KOR'],
+    ['AAW','AFRIQIYAH','Afriqiyah Airways','LBY'],
+    ['AAY','ALLEGIANT','Allegiant Air','USA'],
+    ['ABB','AIR BELGIUM','Air Belgium SA','BEL'],
+    ['ABD','ATLANTA','Air Atlanta Icelandic','ISL'],
+    ['ABG','ABAKAN','Royal Flight','BLR'],
+    ['ABL','AIR BUSAN','Air Busan','KOR'],
 ];
